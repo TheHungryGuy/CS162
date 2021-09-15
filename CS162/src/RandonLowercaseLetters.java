853c28370da633a78b7c2e860c1be914439db02b
Antonio <Antonio@TheRedBox>
2021-09-15 18:27:05 -0400

    test

---
+++ CS162/src/RandonLowercaseLetters.java
@@ -0,0 +1,25 @@
+
+import java.math.*;
+import java.util.*;
+public class RandonLowercaseLetters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] myList = new int[100];
+		Random rand = new Random();
+		
+		// define the range
+        int max = 10;
+        int min = 1;
+        int range = max - min + 1;
+		
+		for(int i= 0;i < myList.length;i++) {
+			myList[i]=(rand.nextInt(93) + 1);
+			System.out.print(myList[i] + ",");
+		}
+		
+
+	}
+
+}
