4eb844cbc48e16de89d9d35c0569b79abbc821a5
Antonio <Antonio@TheRedBox>
2021-09-15 20:05:44 -0400

    Random Lowercase Project

--- CS162/src/randonLowercaseLetters.java
+++ CS162/src/randonLowercaseLetters.java
@@ -1,25 +1,36 @@
-
-import java.math.*;
 import java.util.*;
 public class randonLowercaseLetters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		int[] myList = new int[100];
+		//Create array list to hold 100 numbers
+		int[] numbersArr = new int[100];
+		//Create array list hold characters
+		char[] charArr = new char[100];
+		//create random class
 		Random rand = new Random();
 		
-		// define the range
-        int max = 10;
-        int min = 1;
-        int range = max - min + 1;
-		
-		for(int i= 0;i < myList.length;i++) {
-			myList[i]=(rand.nextInt(93) + 1);
-			System.out.print(myList[i] + ",");
+		//Run for loop to assign 100 random numbers
+		for(int i= 0;i < numbersArr.length;i++) {
+			
+			//Subtract min from max to set range
+			// then add min number 
+			numbersArr[i]=(rand.nextInt(26) + 97); 
+			//Print out all numbers
+			System.out.print(numbersArr[i] + ",");
+		}
+		//
+		System.out.println();
+		System.out.println();
+		//Run for loop to take numbers from last array
+		//insert into new char array
+		for (int i=0; i< charArr.length;i++) {
+		//typecast them into char to convert to ascii 
+			charArr[i]= (char) numbersArr[i];
+		//Print out all characters	
+			System.out.println((i+1) +"." + charArr[i]);
+			
 		}
 		
-
 	}
-
 }
